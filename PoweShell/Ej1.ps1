930faebc67f159c66d041445689eae9aa03d407e
CarlosRieraFerrer <Carlosrieraferrer11@gmail.com>
2024-12-19 11:59:36 +0100

    Ej1

---
+++ PoweShell/Ej1.ps1
@@ -0,0 +1,9 @@
+﻿$nombreUsuario = $env:USERNAME
+$directorioTrabajo = $PWD.Path
+$nombreDominio = $env:USERDOMAIN
+$nombreEquipo = $env:COMPUTERNAME
+
+write-Output "Hola $nombreUsuario"
+write-Output "Tu directorio de trabajo es $directorioTrabajo"
+Write-Output "perteneces al dominio $nombreDominio"
+Write-Output "Tu equipo se llama $nombreEquipo"
---
+++ PoweShell/Ej2.ps1
@@ -0,0 +1,25 @@
+﻿Write-Host "Introduce el primer número:"
+$num1 = Read-Host
+
+Write-Host "Introduce el segundo número:"
+$num2 = Read-Host
+
+$num1 = [float]$num1
+$num2 = [float]$num2
+
+$suma = $num1 + $num2
+$resta = $num1 - $num2
+$multiplicacion = $num1 * $num2
+if ($num2 -ne 0) {
+    $division = $num1 / $num2
+    $resto = $num1 % $num2
+} else {
+    $division = "No se puede dividir por cero"
+    $resto = "No se puede calcular el resto con divisor cero"
+}
+
+Write-Output "La suma de $num1 y $num2 es: $suma"
+Write-Output "La resta de $num1 y $num2 es: $resta"
+Write-Output "La multiplicación de $num1 y $num2 es: $multiplicacion"
+Write-Output "La división de $num1 entre $num2 es: $division"
+Write-Output "El resto de $num1 entre $num2 es: $resto"
