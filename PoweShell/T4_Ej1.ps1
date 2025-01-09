a8ec41aaa430c81326baf91d8c6720bc4afa8384
CarlosRieraFerrer <Carlosrieraferrer11@gmail.com>
2025-01-09 12:24:40 +0100

    Ej1

---
+++ PoweShell/T4_Ej1.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "Por favor, introduce tu nombre:"
+$nombre = Read-Host
+
+Write-Host "Por favor introduce un numero:"
+$numero = Read-Host
+
+for ($i = 1; $i -le [int]$numero; $i++) {
+    Write-Host $nombre
+    }
